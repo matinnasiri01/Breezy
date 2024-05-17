a6181eac3422d666928af6ea460956b4d012ebd7
Matin <mnasirii829@gmail.com>
2024-05-17 20:05:36 +0330

    Added Progress

---
+++ app/src/main/java/me/nasiri/breezy/presentation/ui/elements/Progress.kt
@@ -0,0 +1,25 @@
+package me.nasiri.breezy.presentation.ui.elements
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun Progress() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+        content = { CircularProgressIndicator() }
+    )
+}
+
+
+@Preview
+@Composable
+private fun ProgressPreview() {
+    Progress()
+}
