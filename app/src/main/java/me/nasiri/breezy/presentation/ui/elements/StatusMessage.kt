964338d2e0b8724a6c9575f2536a162d2f7f357b
Matin <mnasirii829@gmail.com>
2024-05-17 20:24:10 +0330

    Added status message

---
+++ app/src/main/java/me/nasiri/breezy/presentation/ui/elements/StatusMessage.kt
@@ -0,0 +1,44 @@
+package me.nasiri.breezy.presentation.ui.elements
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import me.nasiri.breezy.R
+
+@Composable
+fun StatusMessage(/* todo 1.get color from state 2.get message! */) {
+    val containerColor = Color.Yellow
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(containerColor, RoundedCornerShape(12.dp))
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(painter = painterResource(id = R.drawable.ic_warning), contentDescription = null)
+        Spacer(modifier = Modifier.width(4.dp))
+        Text(text = stringResource(id = R.string.st_internet), maxLines = 1, fontWeight = FontWeight.Bold)
+    }
+}
+
+
+@Preview
+@Composable
+private fun StatusMessagePreview() {
+    StatusMessage()
+}
