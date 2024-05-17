85755eda9e7b3dad722b2556c5764fe4aae0a103
Matin <mnasirii829@gmail.com>
2024-05-17 19:57:17 +0330

    Added header

---
+++ app/src/main/java/me/nasiri/breezy/presentation/ui/elements/Header.kt
@@ -0,0 +1,54 @@
+package me.nasiri.breezy.presentation.ui.elements
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import me.nasiri.breezy.R
+
+@Composable
+fun Header(/*todo 1.get bg color 2.get text from state*/) {
+    val bgColor = Color.Red
+    Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = stringResource(id = R.string.sp_city),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        Text(
+            color = bgColor,
+            text = stringResource(id = R.string.sp_time_header),
+            modifier = Modifier
+                .background(Color.Black, RoundedCornerShape(24.dp))
+                .padding(horizontal = 16.dp, vertical = 4.dp)
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(text = stringResource(id = R.string.sp_status))
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = stringResource(id = R.string.sp_centigrade_header),
+            fontSize = 72.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun HeaderPreview() {
+    Header()
+}
